7eb3618cda253afa9978426a2a519501b416f54b
vicente <vi.provoste@duocuc.cl>
2026-04-14 13:16:30 -0400

    primer commit

---
+++ vicente/src/main/java/com/provoste/vicente/VicenteApplication.java
@@ -0,0 +1,13 @@
+package com.provoste.vicente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VicenteApplication.class, args);
+	}
+
+}
---
+++ vicente/src/test/java/com/provoste/vicente/VicenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provoste.vicente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VicenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
